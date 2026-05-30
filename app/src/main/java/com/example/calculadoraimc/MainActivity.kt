6ea4d7a6c73e91b0061143ec7ab8825bb3e22a78
MasalemaCristiano <kevinfuela175@gmail.com>
2026-05-29 23:53:21 -0500

    Pantalla de ingreso de datos

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -2,11 +2,72 @@ package com.example.calculadoraimc
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            MaterialTheme {
+                CalculadoraIMC()
+            }
+        }
+    }
+}
+
+@Composable
+fun CalculadoraIMC() {
+    PantallaInicio()
+}
+
+@Composable
+fun PantallaInicio() {
+
+    var nombre by remember { mutableStateOf("") }
+    var peso by remember { mutableStateOf("") }
+    var altura by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text("Calculadora de IMC")
+
+        OutlinedTextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            label = { Text("Nombre") }
+        )
+
+        OutlinedTextField(
+            value = peso,
+            onValueChange = { peso = it },
+            label = { Text("Peso") }
+        )
+
+        OutlinedTextField(
+            value = altura,
+            onValueChange = { altura = it },
+            label = { Text("Altura") }
+        )
+
+        Button(onClick = {}) {
+            Text("Calcular")
+        }
     }
 }
